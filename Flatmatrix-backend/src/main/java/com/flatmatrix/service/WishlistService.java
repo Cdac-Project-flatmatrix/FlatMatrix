eb50774be958f50583c461eebb44a89d7cad6d02
Snehal Patil <patilsnehaljagannath@gmail.com>
2025-02-05 22:29:14 +0530

    get wishlisted properties

--- Flatmatrix-backend/src/main/java/com/flatmatrix/service/WishlistService.java
+++ Flatmatrix-backend/src/main/java/com/flatmatrix/service/WishlistService.java
@@ -1,21 +1,28 @@
 package com.flatmatrix.service;
 
 import com.flatmatrix.repositories.WishlistRepository;
+
+import jakarta.transaction.Transactional;
+
 import com.flatmatrix.repositories.UserRepository;
 import com.flatmatrix.repositories.PropertyRepository;
 import com.flatmatrix.entities.Wishlist;
 import com.flatmatrix.entities.User;
 import com.flatmatrix.entities.Property;
 import com.flatmatrix.custom_exception.ResourceNotFoundException;
-
+import com.flatmatrix.dto.PropertyResponseDto;
+import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class WishlistService {
 
     private static final Logger logger = LoggerFactory.getLogger(WishlistService.class);
@@ -26,6 +33,9 @@ public class WishlistService {
     private UserRepository userRepository;
     @Autowired
     private PropertyRepository propertyRepository;
+    
+    @Autowired
+    private ModelMapper modelMapper;
     
     public String addToWishlist(Long propertyId, Long userId) {
         logger.info("Adding property {} to wishlist for user {}", propertyId, userId);
@@ -62,5 +72,18 @@ public class WishlistService {
         wishlistRepository.delete(wishlist);
         logger.info("Property {} removed from wishlist for user {}", propertyId, userId);
         return "Property removed from wishlist successfully.";
+    }
+    
+    public List<PropertyResponseDto> getWishlist(Long userId) {
+        logger.info("Fetching wishlist for user {}", userId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with id " + userId));
+        List<Wishlist> wishlistItems = wishlistRepository.findByUser(user);
+        List<Property> properties = wishlistItems.stream()
+                .map(Wishlist::getProperty)
+                .collect(Collectors.toList());
+        return properties.stream()
+                .map(property -> modelMapper.map(property, PropertyResponseDto.class))
+                .collect(Collectors.toList());
     }
 }
